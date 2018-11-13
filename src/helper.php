515101069935fb98e929d19fa73cade6aa3cf638
Mark Villudo <markanthony.villudo@gmail.com>
2018-11-13 12:08:01 +0800

    Update Helper
    Additional fields (platform, device and browser)

--- src/helper.php
+++ src/helper.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\ActivityLog;
+use Jenssenger\Agent\Agent;
 
 if (!function_exists('storeActivity')) {
     /**
@@ -19,11 +20,19 @@ if (!function_exists('storeActivity')) {
         if (Auth::check()) {
             $userId = auth()->user()->id;
         }
+        $agent = new Agent();
+        
+        $platform = $agent->platform();
+        $device = $agent->device();
+        $browser = $agent->$browser();
         
         //Activity logs
         $activity = new ActivityLog;
         $activity->type = $type; //Payment //Orders //Others
         $activity->user_id = $userId;
+        $activity->platform = $platform;
+        $activity->device = $device;
+        $activity->browser = $browser;
         $activity->action = $action; //Edit user, add order, deliverred order, etc
         $activity->description = $description; //message
         $activity->ip_address = request()->ip(); //ip_address of a user
